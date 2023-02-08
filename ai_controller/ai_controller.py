eb3cf32090d4083805f50123c740d9a5b3bf6806
nesl <kiototeko@gmail.com>
2023-02-08 14:53:08 -0800

    add info

--- ai_controller/ai_controller.py
+++ ai_controller/ai_controller.py
@@ -27,6 +27,33 @@ args = parser.parse_args()
 env = gym.make('gym_collab/AICollabWorld-v0', use_occupancy=args.use_occupancy, view_radius=args.view_radius, client_number=int(args.robot_number), host=args.host, port=args.port, address=args.address, cert_file=args.cert_file, key_file=args.key_file)
 
 
+'''
+ACTIONS
+
+move_up = 0
+move_down = 1
+move_left = 2
+move_right = 3
+move_up_right = 4
+move_up_left = 5
+move_down_right = 6
+move_down_left = 7
+grab_up = 8
+grab_right = 9
+grab_down = 10
+grab_left = 11
+grab_up_right = 12
+grab_up_left = 13
+grab_down_right = 14
+grab_down_left = 15
+drop_object = 16
+danger_sensing = 17
+
+OBSERVATIONS
+
+Occupancy map
+Objects held
+'''
 
 
 observation, info = env.reset()
